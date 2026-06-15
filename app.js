aad6c338b8e6dbff49229a66f6426091cec08942
madalapranavsai <madalapranavsai@gmail.com>
2026-06-15 15:58:23 +0530

    task9

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Task 6 is running!'));
-app.listen(3000, () => console.log('Listening on 3000'));
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello from the Dev Environment! Version 2</h1>');
+});
+
+app.listen(port, () => {
+  console.log(`Dev app listening on port ${port}`);
+});
